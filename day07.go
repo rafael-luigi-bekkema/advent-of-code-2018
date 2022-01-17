e0586ae49bddaafcb5becd321c5709553a91e392
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2022-01-17 19:07:46 +0100

    Day 7

--- day07.go
+++ day07.go
@@ -1,15 +1,84 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func day7parse(input []string) map[byte][]byte {
+	deps := map[byte][]byte{}
+	for _, line := range input {
+		var s [2]byte
+		Must(fmt.Sscanf(line, "Step %c must be finished before step %c can begin.", &s[0], &s[1]))
+		deps[s[1]] = append(deps[s[1]], s[0])
+		if _, ok := deps[s[0]]; !ok {
+			deps[s[0]] = nil
+		}
+	}
+	return deps
+}
+
+func day7next(deps map[byte][]byte, done map[byte]bool) byte {
+	var dones []byte
+deps:
+	for item, ideps := range deps {
+		for _, dep := range ideps {
+			if !done[dep] {
+				continue deps
+			}
+		}
+		dones = append(dones, item)
+	}
+	if len(dones) == 0 {
+		return 0
+	}
+	item := Min(dones...)
+	delete(deps, item)
+	return item
+}
 
 func day7a(input []string) string {
-	// "Step F must be finished before step E can begin.",
-	items := make([][2]byte, len(input))
-	for i, line := range input {
-		s := &items[i]
-		Must(fmt.Sscanf(line, "Step %c must be finished before step %c can begin.", &s[0], &s[1]))
-
+	deps := day7parse(input)
+	done := map[byte]bool{}
+	result := make([]byte, 0, len(deps))
+	for len(deps) > 0 {
+		item := day7next(deps, done)
+		done[item] = true
+		result = append(result, item)
 	}
-	fmt.Println(items)
-	return ""
+	return string(result)
+}
+
+func day7b(input []string, sec, elfs int) int {
+	s := -1
+	deps := day7parse(input)
+
+	type Work struct {
+		what   byte
+		doneAt int
+	}
+	done := map[byte]bool{}
+	workers := map[int]*Work{}
+	result := make([]byte, 0, len(deps))
+	for len(deps) > 0 || len(workers) > 0 {
+		s++
+		for elf, work := range workers {
+			if work.doneAt == s {
+				done[work.what] = true
+				result = append(result, work.what)
+				delete(workers, elf)
+			}
+		}
+		for elf := 0; elf < elfs; elf++ {
+			work := workers[elf]
+			if work == nil {
+				item := day7next(deps, done)
+				if item == 0 {
+					continue
+				}
+				d := s + int(item-'A') + sec + 1
+				workers[elf] = &Work{what: item, doneAt: d}
+			}
+		}
+	}
+	return s
 }
--- day07_test.go
+++ day07_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestDay7(t *testing.T) {
-	TestEqual(t, "CABDFE", day7a([]string{
+	example := []string{
 		"Step C must be finished before step A can begin.",
 		"Step C must be finished before step F can begin.",
 		"Step A must be finished before step B can begin.",
@@ -11,5 +11,10 @@ func TestDay7(t *testing.T) {
 		"Step B must be finished before step E can begin.",
 		"Step D must be finished before step E can begin.",
 		"Step F must be finished before step E can begin.",
-	}))
+	}
+	TestEqual(t, "CABDFE", day7a(example))
+	TestEqual(t, 15, day7b(example, 0, 2))
+	file := Lines(7)
+	TestEqual(t, "HEGMPOAWBFCDITVXYZRKUQNSLJ", day7a(file))
+	TestEqual(t, 1226, day7b(file, 60, 5))
 }
